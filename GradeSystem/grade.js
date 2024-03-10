432dfebc1419702221178cf737f045cc294f8b67
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-03-11 02:26:38 +0500

    Add files via upload

---
+++ GradeSystem/grade.js
@@ -0,0 +1,19 @@
+var grade = 70;
+if (grade >= 90) {
+    console.log("Your Grade is A+ Fantastic ".concat(grade));
+}
+else if (grade >= 80) {
+    console.log("Your Grade is A Exellent ".concat(grade));
+}
+else if (grade >= 70) {
+    console.log("Your Grade is B Good ".concat(grade));
+}
+else if (grade >= 50) {
+    console.log("Your Grade is C Poor ".concat(grade));
+}
+else if (grade >= 40) {
+    console.log("Your Grade is D Pass ".concat(grade));
+}
+else {
+    console.log("You Are Fail ".concat(grade));
+}
---
+++ GradeSystem/grade.ts
@@ -0,0 +1,16 @@
+let grade = 70;
+
+if (grade >= 90) {
+    console.log(`Your Grade is A+ Fantastic ${grade}`)
+} else if (grade >= 80) {
+    console.log(`Your Grade is A Exellent ${grade}`)
+} else if (grade >= 70) {
+    console.log(`Your Grade is B Good ${grade}`)
+} else if (grade >= 50) {
+    console.log(`Your Grade is C Poor ${grade}`)
+} else if (grade >= 40) {
+    console.log(`Your Grade is D Pass ${grade}`)
+}
+else {
+    console.log(`You Are Fail ${grade}`)
+}
